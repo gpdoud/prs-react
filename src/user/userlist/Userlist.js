718237e3c06c16f2355193114477b50ffe45b31c
Greg Doud <gpdoud@gmail.com>
2022-11-20 14:56:03 -0500

    Chg Userlist from class to func component

--- src/user/userlist/Userlist.js
+++ src/user/userlist/Userlist.js
@@ -1,101 +1,69 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Menu from '../../Menu';
-import './Userlist.css';
 import Header from '../../header';
+import Menu from '../../Menu';
 
-class Userlist extends React.Component {
+const uri = "http://localhost:5000/api/users";
 
-    constructor(props) {
-        super(props);
-        this.state = { 
-            error : null,
-            isLoaded : false,
-            users : [] 
-        }
-        this.componentDidMount = this.componentDidMount.bind(this);
+const callUserList = async () => {
+    const res = await fetch(uri);
+    const users = await res.json();
+    console.debug("Users:", users);
+    return users;
+}
+
+function Userlist() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        callUserList()
+            .then(res => setUsers(res))
+    }, []);
+
+    let trs = [];
+    for (let u of users) {
+        trs.push(
+            <tr>
+                <td>{u.id}</td>
+                <td>{u.username}</td>
+                <td>{u.firstname}</td>
+                <td>{u.lastname}</td>
+                <td>{u.phone ?? "(null)"}</td>
+                <td>{u.email && "(null)"}</td>
+                <td>{u.isReviewer ? "Y" : "N"}</td>
+                <td>{u.isAdmin ? "Y" : "N"}</td>
+                <td>
+                    <Link to={`/user/detail/${u.id}`}>Detail</Link>
+                    <span> | </span>
+                </td>
+            </tr>
+        )
     }
 
-    componentDidMount() {
-        fetch("http://localhost:5000/api/users")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.debug("Sucessful AJAX call!", result);
-                    this.setState({ 
-                        isLoaded: true,
-                        users: result
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        error,
-                        isLoaded: false
-                    });
-                }
-            );
-    
-    }
-
-    generateRows() {
-        const users = this.state.users;
-        let trs = [];
-        users.forEach((u,i) => {
-            let tr = (
-                <tr key={'tr'+i}>
-                    <td>{u.id}</td>
-                    <td>{u.username}</td>
-                    <td>{u.firstname}</td>
-                    <td>{u.lastname}</td>
-                    <td>{u.isReviewer ? "Yes" : "No"}</td>
-                    <td>{u.isAdmin ? "Yes" : "No"}</td>
-                    <td>
-                        <Link to={`/user/detail/${u.id}`}>Detail</Link>
-                        <span> | </span>
-                        <span>Edit</span>
-                    </td>
-                </tr>
-            )
-            trs.push(tr);
-        })
-        return trs;
-    }
-
-    render() {
-        const isLoaded = this.state.isLoaded;
-        const error = this.state.error;
-        if(error) { 
-            console.error(error);
-            return <p>Error</p>
-        } else if(!isLoaded) {
-            return <p>Loading ...</p>
-        } else {
-            const rows = this.generateRows();
-            return (
-                <Fragment>
-                    <Menu />
-                    <Header pageTitle="User List" linkComponent="ABC" linkDisplay="XYZ" />
-                    <Link to={"/user/create"}>Create New</Link>
-                    <table className="table table-sm">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Username</th>
-                                <th>Firstname</th>
-                                <th>Lastname</th>
-                                <th>Reviewer</th>
-                                <th>Admin</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {rows}
-                        </tbody>
-                    </table>
-                </Fragment>
-            )
-        }
-    }
+    return (
+        <main>
+            <Menu />
+            <Header pageTitle="User List" />
+            <table className='table table-sm'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>Reviewer?</th>
+                        <th>Admin?</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {trs}
+                </tbody>
+            </table>
+        </main>
+    );
 }
 
 export default Userlist;
